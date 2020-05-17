53e8738971deb27c281be2d5ec8695fbda6c2227
goldnotch <goldnotch@gmail.com>
2020-05-17 20:11:43 +0500

    изменил Program

--- KeyboardSmasher/GUI/MainWindow.Designer.cs
+++ KeyboardSmasher/GUI/MainWindow.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainWindow";
+            this.Name = "MainWindow";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
