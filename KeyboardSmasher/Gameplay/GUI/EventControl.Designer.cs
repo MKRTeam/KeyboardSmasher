1eb70c8995965132b39cbdf546ba380e45dc13e7
goldnotch <goldnotch@gmail.com>
2020-05-17 19:46:54 +0500

    добавлена папка data

--- KeyboardSmasher/Gameplay/GUI/EventControl.Designer.cs
+++ KeyboardSmasher/Gameplay/GUI/EventControl.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // EventControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "EventControl";
+            this.Size = new System.Drawing.Size(310, 368);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
