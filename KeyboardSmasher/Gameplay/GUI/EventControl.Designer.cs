15ff7cab39ad869c64b8f15c9bd77ec328e9c85f
Flatren <45997535+Flatren@users.noreply.github.com>
2020-05-18 21:22:46 +0500

    EventControl - вставка форм

--- KeyboardSmasher/Gameplay/GUI/EventControl.Designer.cs
+++ KeyboardSmasher/Gameplay/GUI/EventControl.Designer.cs
@@ -28,18 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(538, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(232, 332);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(532, 332);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(6, 341);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(767, 215);
+            this.panel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(767, 215);
+            this.tableLayoutPanel1.TabIndex = 0;
             // 
             // EventControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "EventControl";
-            this.Size = new System.Drawing.Size(310, 368);
+            this.Size = new System.Drawing.Size(773, 556);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- KeyboardSmasher/Gameplay/GUI/EventControl.cs
+++ KeyboardSmasher/Gameplay/GUI/EventControl.cs
@@ -12,9 +12,27 @@ namespace KeyboardSmasher.Gameplay.GUI
 {
     public partial class EventControl : UserControl
     {
-        public EventControl()
+        string name_image;
+        string[] action;
+        string textScene;
+        public string Result;
+        public EventControl(string name_image,string[] actions,string textScene)
         {
             InitializeComponent();
+            tableLayoutPanel1.RowCount = action.Length;
+            for (int i = 0; i < action.Length; i++)
+            {
+                Button button = new Button();
+                button.Dock = DockStyle.Fill;
+                button.Text = actions[i];
+                button.Click += OnClickButton_Action;
+                tableLayoutPanel1.SetRow(button, i);
+            }
         }
+        void OnClickButton_Action(object sender, EventArgs e)
+        {
+             
+        }
+        
     }
 }
