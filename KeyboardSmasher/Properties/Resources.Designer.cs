9b1f7ed33810cb27e040952ad7f55581d66e70a1
koffe <niccolum129@gmail.com>
2020-06-03 19:19:57 +0500

    небольшие исправления и добавления для #9: назначение иконки, добавление фона на форму, замена #LANGUAGE и #DIFFICULTY для роботов на человеческие слова для человеков

--- KeyboardSmasher/Properties/Resources.Designer.cs
+++ KeyboardSmasher/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace KeyboardSmasher.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap keys_background_1024x768 {
+            get {
+                object obj = ResourceManager.GetObject("keys_background_1024x768", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap mistakeCountArrows {
             get {
                 object obj = ResourceManager.GetObject("mistakeCountArrows", resourceCulture);
